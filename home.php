7baed8cb6a5e65283994a840118eba734f97d21b
DESKTOP-UFA5ELE\diogo <diogofernandoneves1@gmail.com>
2023-05-08 19:17:37 +0100

    Falta meter as frases começarem todas na esquerda do ecrã e falta a parte do login

--- home.php
+++ home.php
@@ -4,45 +4,49 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
-  <div class="fade-half">
-    <video id="Background-video" autoplay loop muted >
-    <source src="imagens/video.mp4" type="video/mp4">
-    </video>
-  </div>
   <head>
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
     <link href="css/csshome.css" rel="stylesheet">
     <!-- insert here the reference to stylesheet file -->
     <title>Opportunity</title>
-
-    
   </head>
-<body>
+  <body>
+  <div class="texto1">
+      <p>Bem vindo á comunidade Opportunity onde poderás te logar ou criar conta caso ainda não tenhas criado</p>
+      <p>Este site tem como função apresentação de jogos online tal como a criação de uma equipa e gestão da mesma<p>
+      <p>Em caso de duvidas podes nos contactar as informações estão no fundo do ecrã<p>
+    </div> 
+    <div class="fade-half">
+      <video id="Background-video" autoplay loop muted >
+      <source src="imagens/video.mp4" type="video/mp4">
+      </video>
+    </div>
+  
 
+    <header>
+      <div class="cabecalho">
+        <div class="logo">
+          <img src="imagens/Logo_Opportunity3.png" class="fixed-image">
+        </div>
+        <ul class="nav-area">
+          <a href="login.php" class="button button-login">Login</a>
+          <li><a href="create.php" class="button">Register</a></li>
+        </ul>
+      </div>  
 
-  <header>
-    <div class="cabecalho">
-      <div class="logo">
-        <img src="imagens/Logo_Opportunity3.png">
-      </div>
-      <ul class="nav-area">
-      <ul>
-            <li>
-              <a href="create.php">Registar</a>
-            </li>
-          </ul>
-      </ul>
-    </div>  
       <div class="Bem-vindo">
         <h1>Bem vindo ao Opportunity</h1>
-      </div>  
+      </div>   
       <div class="Info">
-      <p id="contactos">Contacta-nos: diogofernandoneves1@gmail.com / ICSF2442@gmail.com<p>
+        <p id="contactos">Contacta-nos: diogofernandoneves1@gmail.com / ICSF2442@gmail.com</p>
       </div>
-  </header>
-</body>
+    </header>
+  </body>
+</html>
+
+
   
 
