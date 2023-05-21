0be9989a5429218de971df09bcb07f86b5b361b7
DESKTOP-UFA5ELE\diogo <diogofernandoneves1@gmail.com>
2023-05-21 15:25:31 +0100

    Confirmação de e-mail feita front-end

---
+++ Confirmação.php
@@ -0,0 +1,46 @@
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+    <title>Opportunity</title>
+</head>
+<body>
+<div class="container py-0 p-2 sticky-top">
+    <div class="row">
+        <div class="col">
+            <nav aria-label="breadcrumb" class="bg-light rounded-3 p-3 mb-4" style="padding: 5px!important; ">
+
+                <ol class="breadcrumb mb-0" style="background-color: midnightblue; align-items: center; display: flex;">
+                    <li class="breadcrumb-item"><a href="home.php">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Verificação e-mail</li>
+
+
+
+
+                </ol>
+          </nav>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="Confirmation">Um código foi enviado para o e-mail do registro insira no campo abaixo:</label>
+                        <input type="text" class="form-control" id="Confirmation" placeholder="Insira o código" required>
+                        <div style="visibility: hidden">
+                            <p4> Isto é um espaço</p4>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
